3379913f6d9f094f11f23543b846f39b59060b29
saktiworkstation <saktiworkstation@gmail.com>
2024-02-05 16:22:42 +0700

    Added the "top up and money withdrawal" function on the controller

--- app/Http/Controllers/TopUpAndWithdrawal.php
+++ app/Http/Controllers/TopUpAndWithdrawal.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class TopUpAndWithdrawal extends Controller
 {
-    //
+    public function index(){
+        // halaman untuk menampilkan form top up dan withdrawl
+    }
+
+    public function storeTopUp(){
+        // fungsi untuk menyimpan data top up
+    }
+
+    public function storeWithdrawal(){
+        // fungsi untuk menyimpan data withdrawal
+    }
 }
