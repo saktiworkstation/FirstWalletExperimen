fdc1eca438988ddd9b360ad10f1660c0ef54de95
saktiworkstation <saktiworkstation@gmail.com>
2024-02-10 17:00:55 +0700

    Create a join between Beneficiaries and Wallet

--- app/Models/Beneficiaries.php
+++ app/Models/Beneficiaries.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Beneficiaries extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function Wallet(){
+        return $this->belongsTo(Wallet::class, 'wallet_id');
+    }
 }
--- app/Models/Wallet.php
+++ app/Models/Wallet.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Beneficiaries;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Wallet extends Model
 {
@@ -18,5 +19,9 @@ class Wallet extends Model
 
     public function Owner(){
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function Beneficiaries() {
+        return $this->hasMany(Beneficiaries::class);
     }
 }
