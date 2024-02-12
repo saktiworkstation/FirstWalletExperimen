5b1ed10dc8ad81900051148847bc69d29d78f580
saktiworkstation <saktiworkstation@gmail.com>
2024-02-12 13:45:36 +0700

    Change the table structure and factory beneficiaries

--- database/factories/BeneficiariesFactory.php
+++ database/factories/BeneficiariesFactory.php
@@ -17,6 +17,7 @@ class BeneficiariesFactory extends Factory
     public function definition(): array
     {
         return [
+            'user_id' => mt_rand(1, 5),
             'wallet_id' => mt_rand(1, 5),
         ];
     }
--- database/migrations/2024_02_09_141601_create_beneficiaries_table.php
+++ database/migrations/2024_02_09_141601_create_beneficiaries_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('beneficiaries', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id');
             $table->foreignId('wallet_id');
             $table->timestamps();
         });
